4099ced74b7e9bcd1196f34e4907aad6f80b7004
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2023-01-01 02:25:47 -0300

    Add style to Projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -4,41 +4,25 @@ import "./Portfolio.css";
 const Projects = () => {
   return (
     <Fragment>
-      <div className="portfolio-container">
-        <div className="projects-container-big">
-          <div className="projects-image">IMG</div>
-          <h1 className="projects-text">Some project name</h1>
-          <h2 className="projects-subtext">Web Development</h2>
+      <section className="portfolio-container">
+        <div className="project-big">
+            <h1 className="projects-text">Algum texto</h1>
+            <h2 className="projects-subtext">Alguma descricao</h2>
         </div>
         <div className="projects-container">
-          <div className="projects-image">IMG</div>
-          <h1 className="projects-text">Some project name</h1>
-          <h2 className="projects-subtext">Web Development</h2>
+          <div className="project-small"></div>
+          <div className="project-small"></div>
+        </div>
+        <div className="project-long"></div>
+        <div className="projects-container">
+          <div className="project-small"></div>
+          <div className="project-small"></div>
         </div>
         <div className="projects-container">
-          <div className="projects-image">IMG</div>
-          <h1 className="projects-text">Some project name</h1>
-          <h2 className="projects-subtext">Web Development</h2>
+          <div className="project-small"></div>
+          <div className="project-small"></div>
         </div>
-      </div>
-      <div className="portfolio-container">
-        <div className="projects-container">
-          <div className="projects-image">IMG</div>
-          <h1 className="projects-text">Some project name</h1>
-          <h2 className="projects-subtext">Web Development</h2>
-        </div>
-        <div className="projects-container">
-          <div className="projects-image">IMG</div>
-          <h1 className="projects-text">Some project name</h1>
-          <h2 className="projects-subtext">Web Development</h2>
-        </div>
-        <div className="projects-container-big">
-          <div className="projects-image">IMG</div>
-          <h1 className="projects-text">Some project name</h1>
-          <h2 className="projects-subtext">Web Development</h2>
-        </div>
-      </div>
-      awdwadwa
+      </section>
     </Fragment>
   );
 };
