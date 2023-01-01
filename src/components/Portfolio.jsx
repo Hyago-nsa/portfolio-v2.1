b6815002f9e20ba1ccf3b4584ff4ce88e9aa4f17
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2023-01-01 03:22:55 -0300

    Add style to Projects

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,21 +6,42 @@ const Projects = () => {
     <Fragment>
       <section className="portfolio-container">
         <div className="project-big">
+          <h1 className="projects-text">Algum texto</h1>
+          <h2 className="projects-subtext">Alguma descricao</h2>
+        </div>
+        <div className="projects-container">
+          <div className="project-small">
             <h1 className="projects-text">Algum texto</h1>
             <h2 className="projects-subtext">Alguma descricao</h2>
+          </div>
+          <div className="project-small">
+            <h1 className="projects-text">Algum texto</h1>
+            <h2 className="projects-subtext">Alguma descricao</h2>
+          </div>
+        </div>
+        <div className="project-long">
+          <h1 className="projects-text">Algum texto</h1>
+          <h2 className="projects-subtext">Alguma descricao</h2>
         </div>
         <div className="projects-container">
-          <div className="project-small"></div>
-          <div className="project-small"></div>
+          <div className="project-small">
+            <h1 className="projects-text">Algum texto</h1>
+            <h2 className="projects-subtext">Alguma descricao</h2>
+          </div>
+          <div className="project-small">
+            <h1 className="projects-text">Algum texto</h1>
+            <h2 className="projects-subtext">Alguma descricao</h2>
+          </div>
         </div>
-        <div className="project-long"></div>
         <div className="projects-container">
-          <div className="project-small"></div>
-          <div className="project-small"></div>
-        </div>
-        <div className="projects-container">
-          <div className="project-small"></div>
-          <div className="project-small"></div>
+          <div className="project-small">
+            <h1 className="projects-text">Algum texto</h1>
+            <h2 className="projects-subtext">Alguma descricao</h2>
+          </div>
+          <div className="project-small">
+            <h1 className="projects-text">Algum texto</h1>
+            <h2 className="projects-subtext">Alguma descricao</h2>
+          </div>
         </div>
       </section>
     </Fragment>
