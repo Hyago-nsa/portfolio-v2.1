6f2b4ab9b6f5e3981af40c7ae734d01e8ed0e591
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2023-01-04 01:44:19 -0300

    Changed global colors

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -14,16 +14,57 @@ const Contact = () => {
         </div>
         <div className="contact-info-socialmedia">
           <p>hyago.eurico.nsa@gmail.com</p>
-          <li>Linkedin</li>
-          <li>Github</li>
+          <li>
+            <h1>Linkedin</h1>
+          </li>
+          <li>
+            <h1>Github</h1>
+          </li>
         </div>
       </div>
       <div className="contact-form">
-        <div className="box-long">awdwadaw</div>
-        <div className="box-small-blue">awdwada</div>
-        <div className="box-small-purple">dwadwad</div>
+        <div className="box-long">
+          <textarea
+            id="message"
+            placeholder="Hi, i think we need a Front-end Developer for our team at Company X. How soon can you hop on to discuss this?"
+            rows="7"
+            className=""
+            type="text"
+          ></textarea>
+        </div>
+        <div className="box-small-blue">
+          <input
+            id="name"
+            placeholder="Name"
+            type="text"
+            autoComplete="off"
+          ></input>
+          <input
+            id="email"
+            placeholder="Email"
+            type="text"
+            autoComplete="off"
+          ></input>
+        </div>
+        <div className="box-small-purple">
+          <button className="" type="text">
+            <svg
+              width="300"
+              height="22"
+              viewBox="0 0 72 22"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                class="bow-arrow"
+                fill="none"
+                stroke-width="2"
+                stroke-miterlimit="0"
+                d="M.043 11.119h70.714M60.917 1.319l9.8 9.8-9.8 9.8"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
-
     </section>
   );
 };
