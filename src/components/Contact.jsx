cd29479621f2606546266c8d6db36334690494c1
Hyago-nsa <80161402+Hyago-nsa@users.noreply.github.com>
2023-01-05 16:29:47 -0300

    Add functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,55 +1,106 @@
 import React from "react";
 import "./Contact.css";
+import { useRef } from 'react';
+import emailjs from "@emailjs/browser"
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_h4wbm9d",
+        "template_y0qafaa",
+        form.current,
+        "7CuPXS4xj2MgKMMB1"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+
+    e.target.reset();
+  };
+
   return (
     <section className="contact-container">
       <div className="contact-info">
         <div className="contact-info-text">
-          <p><span className="digital-text">Available</span> for select freelance opportunities</p>
+          <p>
+            <span className="digital-text">Available</span> for select freelance
+            opportunities
+          </p>
         </div>
         <div className="contact-info-subtext">
-          <li>Have an exciting <span className="digital-text">project</span> you need help with?</li>
+          <li>
+            Have an exciting <span className="digital-text">project</span> you
+            need help with?
+          </li>
           <li>Send me an email or contact me via instant message!</li>
         </div>
         <div className="contact-info-socialmedia">
-          <p>hyago.eurico.nsa@gmail.com</p>
+          <p>
+            <a href="mailto:hyago.eurico.nsa@gmail.com?">
+              hyago.eurico.nsa@gmail.com
+            </a>
+          </p>
           <li>
-            <h1>Linkedin</h1>
+            <h1>
+              <a href="https://www.linkedin.com/in/hyago-eurico-421654207/">
+                Linkedin
+              </a>
+            </h1>
           </li>
           <li>
-            <h1>Github</h1>
+            <h1>
+              <a href="https://github.com/Hyago-nsa">Github</a>
+            </h1>
           </li>
         </div>
       </div>
-      <div className="contact-form">
+
+      <form
+        ref={form}
+        onSubmit={sendEmail}
+        className="contact-form"
+      >
         <div className="box-long">
           <textarea
             id="message"
+            name="message"
             placeholder="Hi, i think we need a Front-end Developer for our team at Company X. How soon can you hop on to discuss this?"
             rows="7"
-            className=""
             type="text"
           ></textarea>
         </div>
         <div className="box-small-blue">
           <input
             id="name"
+            name="name"
             placeholder="Name"
             type="text"
             autoComplete="off"
+            required
           ></input>
           <input
             id="email"
+            name="email"
             placeholder="E-mail"
-            type="text"
+            type="email"
             autoComplete="off"
+            required
           ></input>
         </div>
         <div className="box-small-purple">
-          <button className="" type="text">
+          <button type="submit">
             <svg
-            class="bow-arrow"
+              class="bow-arrow"
               width="72"
               height="30"
               viewBox="0 0 72 22"
@@ -65,7 +116,7 @@ const Contact = () => {
             </svg>
           </button>
         </div>
-      </div>
+      </form>
     </section>
   );
 };
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,56 +1,79 @@
 import React, { Fragment } from "react";
 import "./Portfolio.css";
+import "../assets/images/project-dino.png";
 
 const Projects = () => {
   return (
     <Fragment>
       <section className="portfolio-container">
         <div className="project-big">
-          <h1 className="projects-text">Algum texto</h1>
-          <h2 className="projects-subtext">Alguma descricao</h2>
+          <img
+            className="projects-image-big"
+            src="src\assets\images\project-dino.png"
+          />
         </div>
         <div className="projects-container">
           <div className="project-small">
-            <h1 className="projects-text">Algum texto</h1>
-            <h2 className="projects-subtext">Alguma descricao</h2>
+            <img
+              className="projects-image-small"
+              src="src\assets\images\project-ocean.png"
+            />
           </div>
           <div className="project-small">
-            <h1 className="projects-text">Algum texto</h1>
-            <h2 className="projects-subtext">Alguma descricao</h2>
+            <img
+              className="projects-image-small"
+              src="src\assets\images\project-youtube.png"
+            />
           </div>
         </div>
         <div className="project-long">
-          <h1 className="projects-text">Algum texto</h1>
-          <h2 className="projects-subtext">Alguma descricao</h2>
+          <img
+            className="projects-image-long"
+            src="src\assets\images\project-portfolio.png"
+          />
         </div>
         <div className="projects-container">
           <div className="project-small">
-            <h1 className="projects-text">Algum texto</h1>
-            <h2 className="projects-subtext">Alguma descricao</h2>
+            <img
+              className="projects-image-small"
+              src="src\assets\images\project-budget.png"
+            />
           </div>
           <div className="project-small">
-            <h1 className="projects-text">Algum texto</h1>
-            <h2 className="projects-subtext">Alguma descricao</h2>
+            <img
+              className="projects-image-small"
+              src="src\assets\images\project-memory-game.png"
+            />
           </div>
         </div>
         <div className="projects-container">
           <div className="project-small">
-            <h1 className="projects-text">Algum texto</h1>
-            <h2 className="projects-subtext">Alguma descricao</h2>
+            <img
+              className="projects-image-small"
+              src="src\assets\images\project-dino.png"
+            />
           </div>
           <div className="project-small">
-            <h1 className="projects-text">Algum texto</h1>
-            <h2 className="projects-subtext">Alguma descricao</h2>
+            <img
+              className="projects-image-small"
+              src="src\assets\images\project-dino.png"
+            />
           </div>
         </div>
 
         <div className="project-long">
-          <h1 className="projects-text">Algum texto</h1>
-          <h2 className="projects-subtext">Alguma descricao</h2>
+          <img
+            className="projects-image-long"
+            src="src\assets\images\project-dino.png"
+          />
         </div>
         <div className="project-long">
-          <h1 className="projects-text">Algum texto</h1>
-          <h2 className="projects-subtext">Alguma descricao</h2>
+          <img
+            className="projects-image-long"
+            src="src\assets\images\project-dino.png"
+          />
+          {/* <h1 className="projects-text">Algum texto</h1>
+          <h2 className="projects-subtext">Alguma descricao</h2> */}
         </div>
       </section>
     </Fragment>
